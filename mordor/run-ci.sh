02de687dd22bc6ec7752e57b31ec3729c223c863
Truman Zhou <truman.ck.zhou@gmail.com>
2016-01-18 14:45:47 +0800

    devl

--- mordor/run-ci.sh
+++ mordor/run-ci.sh
@@ -1,8 +1,6 @@
-apt-get install -y ragel libboost-dev
-
 
 git clone  https://github.com/mozy/mordor.git
-./buildtools/build.sh --without-zlib
+cd  mordor &&  ./buildtools/build.sh --without-zlib
 
 
 
