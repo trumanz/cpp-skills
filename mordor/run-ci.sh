1a5954e3270bbf1a2aa1809a82107e859d898646
Truman Zhou <truman.ck.zhou@gmail.com>
2016-01-18 15:38:00 +0800

    devl

--- mordor/run-ci.sh
+++ mordor/run-ci.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
 
-git clone  https://github.com/mozy/mordor.git
-cd  mordor &&  ./buildtools/build.sh 
-
-
+if [ -d mordor ]; then
+  echo "use downloaded mordor"
+else
+  git clone  https://github.com/mozy/mordor.git  \
+  && cd  mordor  && mkdir install_dir \
+  &&  ./buildtools/build.sh  --prefix=$(pwd)/install_dir
+  if [ $? != 0 ]; then
+     exit $?
+  fi
+fi
 
--- run-ci.sh
+++ run-ci.sh
@@ -6,7 +6,7 @@ for dir in $(find . -type d  -maxdepth 1 2>/dev/null | grep -v .git   | grep -v 
    cd $currdir
    echo "running ci within $dir"
    cd $dir && ./run-ci.sh
-   if [ $0 != 0]; then
+   if [ $? != 0 ]; then
       exit $0
    fi
 done
