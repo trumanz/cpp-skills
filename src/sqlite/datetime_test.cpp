fae70d337804cfaf5a89dcb4e4e31d47b84e6ff6
Truman Zhou <truman.ck.zhou@gmail.com>
2016-02-26 01:24:06 +0800

    devl

--- src/sqlite/datetime_test.cpp
+++ src/sqlite/datetime_test.cpp
@@ -13,7 +13,7 @@
 //table  id, name, price , time
 
 
-TEST(sqlite, table_search) {
+TEST(sqlite, date_time) {
 
    sqlite3 *db;
   
@@ -22,62 +22,36 @@ TEST(sqlite, table_search) {
    
    const char* sql_create_table = "CREATE TABLE ware(" \
                           "id     INT   PRIMARY KEY NOT NULL," \
-                          "name   TEXT  NOT NULL," \
-                          "price  FLOAT NOT NULL," \
                           "time   DATETIME NOT NULL);";
 
    int rc;
    char *err_msg;
-   rc = sqlite3_exec(db, sql_create_table, NULL, NULL, &err_msg); 
-   if( rc != SQLITE_OK) {
-        printf("SQL error: %s\n", err_msg);
-   }
-
-
-   sqlite3_exec(db, "BEGIN", NULL, NULL, NULL);
-
-   const char *sql_insert = "INSERT INTO ware(id, name, price, time) values(?1, ?2, ?3, ?4);";
    sqlite3_stmt *pstmt;
-   sqlite3_prepare_v2(db, sql_insert, -1, &pstmt, NULL); 
-   for(int i = 0; i < 1000; i++) {
-        int id= i;
-        char name[50]; sprintf(name, "%d", i);
-        float price = i;
-        time_t  time = i;
-        sqlite3_bind_int(    pstmt, 1, id );
-        sqlite3_bind_text(   pstmt, 2, name, -1,  SQLITE_TRANSIENT); //SQLITE_TRANSIENT, sqlite make its own private copy
-        sqlite3_bind_double( pstmt, 3, price );
-        sqlite3_bind_int64(  pstmt, 4, time );
-    
-        rc = sqlite3_step(pstmt);
-        ASSERT_EQ(rc, SQLITE_DONE);
-        sqlite3_reset(pstmt);
-   }
 
-   sqlite3_finalize(pstmt);
-   
-   sqlite3_exec(db, "INSERT INTO ware(id, name, price, time) values('100000', '1000000', '100000', datetime('1970-01-01 00:00:00'))", NULL, NULL, NULL);
-   sqlite3_exec(db, "COMMIT", NULL, NULL, NULL);
+   rc = sqlite3_exec(db, sql_create_table, NULL, NULL, &err_msg); 
+   ASSERT_EQ(SQLITE_OK, rc);
+
+
+   rc = sqlite3_exec(db, "INSERT INTO ware(id, time) values(1, 1)", NULL, NULL, NULL);
+   ASSERT_EQ(SQLITE_OK, rc);
+   rc = sqlite3_exec(db, "INSERT INTO ware(id, time) values(2, 2)", NULL, NULL, NULL);
+   ASSERT_EQ(SQLITE_OK, rc);
+
+   rc = sqlite3_exec(db, "INSERT INTO ware(id, time) values(3, '1970-01-01 00:00:09')", NULL, NULL, NULL);
+   ASSERT_EQ(SQLITE_OK, rc);
+
+
 
    //query all   
    unsigned int count = 0;
-   sqlite3_prepare_v2(db, "SELECT * FROM ware where id = '100000'", -1, &pstmt, NULL);
+   //sqlite3_prepare_v2(db, "SELECT * FROM ware where time > '1970-01-01 00:00:00' ", -1, &pstmt, NULL);
+   sqlite3_prepare_v2(db, "SELECT * FROM ware where time > 2 ", -1, &pstmt, NULL);
    while((rc=sqlite3_step(pstmt)) != SQLITE_DONE) {
-       ASSERT_EQ(rc, SQLITE_ROW);
+       ASSERT_EQ(SQLITE_ROW, rc);
        count++; 
-       int col_count = sqlite3_column_count(pstmt);
-       for(int i = 0; i < col_count; i++) {
-            int type = sqlite3_column_type(pstmt, i);
-            printf("%d, type=%d\n", i, type);
-            const char * columnName = sqlite3_column_name(pstmt, i);
-            if(type == SQLITE_INTEGER) {
-                 int value = sqlite3_column_int(pstmt, i);
-                 printf("%s=%d\n", columnName, value);
-            }
-       }
    }
    sqlite3_finalize(pstmt);
 
-   ASSERT_EQ(1000, count);
+   ASSERT_EQ(2, count);
 }
 
