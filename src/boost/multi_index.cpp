4439590d7996f013c5b55b76548d033e4939ce1f
Truman Zhou <truman.ck.zhou@gmail.com>
2016-02-25 17:45:25 +0800

    devl

--- src/boost/multi_index.cpp
+++ src/boost/multi_index.cpp
@@ -6,6 +6,8 @@
 #include <boost/multi_index/member.hpp>
 #include <boost/multi_index/ordered_index.hpp>
 #include <boost/multi_index/hashed_index.hpp>
+#include <boost/multi_index/sequenced_index.hpp>
+
 
 using namespace std;
 using boost::multi_index_container;
@@ -30,39 +32,70 @@ struct age{};
 typedef multi_index_container<
         employee,
         indexed_by<
+           //sequenced<>,
            hashed_unique< tag<id>, BOOST_MULTI_INDEX_MEMBER(employee, int, id) >,
            ordered_non_unique< tag<name>, BOOST_MULTI_INDEX_MEMBER(employee, std::string, name) >,
            ordered_non_unique< tag<age>, BOOST_MULTI_INDEX_MEMBER(employee, int, age) > >
 > employee_set;
 
 
-TEST(boost_multi_index, hashed_unique_search) {
+TEST(boost_multi_index, hashed_unique_search_and_erase) {
    employee_set es;
-   es.insert(employee(0,"A", 31));
-   es.insert(employee(1,"B", 32 ));
-   es.insert(employee(2,"C", 32 ));
+   es.insert(employee(0,"A", 31) );
+   es.insert(employee(1,"B", 32) );
+   es.insert(employee(2,"C", 32) );
 
    employee_set::iterator it;
+   //search id=1, and got it
    it =   es.get<id>().find(1);
    EXPECT_EQ(1, it->id);
    EXPECT_EQ("B", it->name);
 
-   it =   es.get<id>().find(3);
-    
+   //erase it
+   es.erase(it);
+
+   //then, it dose not exist
+   it =   es.get<id>().find(1);
    EXPECT_EQ(it, es.end());
 
 }
+
+TEST(boost_multi_index, ordered_non_unique_search_between) {
+   employee_set es;
+   es.insert(employee(0,"A", 31));
+   es.insert(employee(4,"E", 34 ));
+   es.insert(employee(1,"F", 32 ));
+   es.insert(employee(2,"F", 32 ));
+   es.insert(employee(3,"F", 33 ));
+
+   for(employee_set::iterator it = es.begin(); it != es.end(); it++) {
+      //  printf("%d\n", it->id);
+   }
+
+   boost::multi_index::index<employee_set,age>::type::iterator  it1;
+   boost::multi_index::index<employee_set,age>::type::iterator  it2;
+   //search age in [32, 34)
+   it1 =  es.get<age>().lower_bound(32);
+   it2 =  es.get<age>().lower_bound(34);
+
+   unsigned int count = 0;
+   for(; it1 != it2; it1++) {
+        count++;
+        EXPECT_EQ(it1->name, "F");
+   }
+
+   EXPECT_EQ(3, count);
+}
+
 
 TEST(boost_multi_index, hashed_unique_descard_new_obj_if_the_same_key) {
    employee_set es;
    
    es.insert(employee(0,"Joe", 31));
    es.insert(employee(0,"Jack", 32 ));
-
    EXPECT_EQ(1,  es.size());
   
    employee_set::iterator it = es.begin();
- 
    EXPECT_EQ(0,  it->id);
    EXPECT_EQ("Joe",  it->name);
    EXPECT_EQ(31,  it->age);
@@ -70,3 +103,26 @@ TEST(boost_multi_index, hashed_unique_descard_new_obj_if_the_same_key) {
 }
 
 
+typedef multi_index_container<
+        employee,
+        indexed_by<
+           sequenced<>,
+           hashed_unique< tag<id>, BOOST_MULTI_INDEX_MEMBER(employee, int, id) >
+        >
+> employee_list;
+
+TEST(boost_multi_index, sequenced_and_hashed_unique_discard_new_obj_if_the_same_key) {
+   employee_list es;
+   es.push_back(employee(0,"A", 1 ));
+   es.push_back(employee(4,"B", 2 ));
+   es.push_back(employee(4,"C", 3 ));//this will be disacrded
+   es.push_back(employee(2,"C", 3 ));
+   es.push_back(employee(3,"D", 4 ));
+ 
+   unsigned int index = 1; 
+   for(employee_list::iterator it = es.begin(); it != es.end(); it++) {
+        EXPECT_EQ(it->age, index++);
+   }
+    
+
+}
