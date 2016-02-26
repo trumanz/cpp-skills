36d1f9cc3971c22870c48388b0f15a189f22000b
Truman Zhou <truman.ck.zhou@gmail.com>
2016-02-26 17:42:51 +0800

    dev

--- src/sqlite/sqlite_helper.h
+++ src/sqlite/sqlite_helper.h
@@ -1,8 +1,11 @@
+
+#ifndef SQLITE_HLPER_H
+#define SQLITE_HLPER_H
+
 #include <sqlite3.h>
 #include <boost/any.hpp>
 
-
-
 void getcolumns(sqlite3_stmt* pstmt, std::vector<boost::any> *values);
-
 void fetch_all_rows(sqlite3_stmt* pstmt, std::vector< std::vector<boost::any> > *rows);
+
+#endif
