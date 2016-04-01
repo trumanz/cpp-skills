fed5d86af9106c06d9344446f0650c320a5f56b3
Truman Zhou <truman.ck.zhou@gmail.com>
2016-04-01 15:27:16 +0800

    dev

--- prep-env.sh
+++ prep-env.sh
@@ -14,7 +14,7 @@ mordor_lib(){
       && cd  mordor  \
       && mkdir install_dir \
       && ./buildtools/build.sh  --prefix=$INSTALL_DIR \
-      && make install
+      && make -j4 && make install
       if [ $? != 0 ]; then
         exit $?
       fi
@@ -28,7 +28,7 @@ gtest(){
      git clone https://github.com/google/googletest.git  \
      && cd googletest \
      && cmake  -DCMAKE_INSTALL_PREFIX:PATH=$INSTALL_DIR \
-     && make install
+     && make -j4 && make  install
      if [ $? != 0 ]; then
         exit $?
      fi
@@ -42,7 +42,7 @@ thrift() {
       git clone https://github.com/apache/thrift.git \
       && cd thrift &&  git checkout  0.9.3 \
       && ./bootstrap.sh &&  ./configure --prefix=$INSTALL_DIR \
-      && make install
+      && make -j4 && make install
       if [ $? != 0 ]; then
         echo "Warning, TODO, fix this "
       fi
