965b63b8d4eaf9b9c51d0e1fd7d1cc7dd1a94051
Truman Zhou <Truman.Zhou@emc.com>
2016-07-26 13:21:08 +0800

    lock

--- src/openmp/parallel_for_basic.cpp
+++ src/openmp/parallel_for_basic.cpp
@@ -5,12 +5,12 @@
 #include "boost/date_time/posix_time/posix_time.hpp"
 #include <cstdio>
 #include <unistd.h>
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 
 using namespace boost::posix_time;
 
-
- 
 TEST(openmp, paralle_for_basic) 
 {
     int count = 128;
@@ -25,10 +25,32 @@ TEST(openmp, paralle_for_basic)
 
 
     ptime t1 =  microsec_clock::local_time();
+
+
+    
+
+    #ifdef _OPENMP
+    omp_lock_t write_lock;
+    omp_init_lock(&writelock);
+    #endif
+
+    int count_sum = 0;
+
     #pragma omp parallel for
     for (int i = 0; i < count;i++) {
            usleep(i*1000);
+           #ifdef _OPENMP
+           omp_set_lock(&writelock);
+           count_sum++;
+           omp_unset_lock(&writelock);
+           #endif
     }
+
+    #ifdef _OPENMP
+    ASSERT_EQ(count_sum, count);
+    #endif
+
+
     time_duration  duration =   microsec_clock::local_time() - t1;
 
     printf("duration: %ld\n", duration.total_milliseconds());
