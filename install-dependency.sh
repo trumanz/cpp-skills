65f4790123894a8c4eb3d5d05ce420d158410780
Truman Zhou <truman.ck.zhou@gmail.com>
2017-07-28 03:56:47 +0000

    fix build issue

---
+++ install-dependency.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update
+sudo apt-get install -y ragel  gawk libtool  cmake  
+sudo apt-get install -y libboost-all-dev libevent-dev 
+sudo apt-get install -y libyaml-dev zlib1g-dev liblzma-dev 
+sudo apt-get install -y postgresql-server-dev-all 
+sudo apt-get install -y libsqlite3-dev
+sudo apt-get install -y libjsoncpp-dev
+sudo apt-get install -y  bison flex
+sudo apt-get install -y ragel
