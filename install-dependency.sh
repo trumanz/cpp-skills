818996b94662c43b152b1e1546777311c984d708
Truman Zhou <truman.ck.zhou@gmail.com>
2019-07-19 14:27:55 +0800

    use gmock

--- install-dependency.sh
+++ install-dependency.sh
@@ -8,5 +8,5 @@ sudo apt-get install -y libsqlite3-dev
 sudo apt-get install -y libjsoncpp-dev
 sudo apt-get install -y  bison flex
 sudo apt-get install -y ragel
-sudo apt-get install -y libgtest-dev
+sudo apt-get install -y libgtest-dev  google-mock
 sudo bash -c  'cd /usr/src/gtest  &&  cmake  CMakeLists.txt &&  make &&  cp *.a  /usr/lib/'
